d8dd379a288baa46e41717f32f62562c5196e579
CL Liu <lcl.berry@gmail.com>
2021-01-15 15:00:43 +0800

    01.15update

--- chapter13_Files.py
+++ chapter13_Files.py
@@ -13,3 +13,19 @@ while True:
     print(theline, end='')
 
 mynewhandle.close()
+
+f = open('test.txt')    # 绝对地址："C:\\temp\\somefile.txt"   相对地址："/home/jimmy/somefile.txt"
+content = f.read()
+f.close()
+
+words = content.split()
+print(f"there are {len(words)} words in the file.")   # 为啥是9个？
+
+
+# copy files from URL
+import urllib.request
+
+url = 'http://www.qingfan.com'
+destination_filename = 'baidu.txt'
+
+urllib.request.urlretrieve(url, destination_filename)
